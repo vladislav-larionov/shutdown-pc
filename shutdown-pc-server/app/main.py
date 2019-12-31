42db86efebe654f15442452ee07ed8b76c8af5b6
Vladislav Larionov <vladlarionov998@gmail.com>
2019-12-31 16:15:48 +0300

    Add the existing shutdown-pc-server

---
+++ shutdown-pc-server/app/main.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask, request
+
+app = Flask(__name__)
+app.secret_key = 'thisMySecretKeyFOrAPP'
+
+
+@app.route('/shutdown')
+def shutdown() -> str:
+    command = 'shutdown /s ' + '/t ' + request.args.get('time')
+    os.system(command)
+    return "True"
+
+
+@app.route('/shutdown_cancel')
+def shutdown_cancel() -> str:
+    command = "shutdown /a"
+    os.system(command)
+    return "True"
+
+
+@app.route('/connect')
+def connect() -> str:
+    return "True"
+
+
+@app.route('/close_server')
+def close_server():
+    os.abort()
+
+
+@app.route('/', methods=['get'])
+def root() -> str:
+    return "True"
+
+
+@app.route('/test')
+def test() -> str:
+    command = request.args.get('test')
+    print(command)
+    return "True"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
