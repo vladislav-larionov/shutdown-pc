77e1c5fe8921e6154308355c3a43a97ec9470d39
Vladislav Larionov <vladlarionov998@gmail.com>
2020-06-03 23:54:06 +0300

    Remake the client with TCP client-server pattern

--- shutdown-pc-android-client/app/src/main/java/com/example/shutdown_pc/MainActivity.java
+++ shutdown-pc-android-client/app/src/main/java/com/example/shutdown_pc/MainActivity.java
@@ -1,45 +1,56 @@
 package com.example.shutdown_pc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.DialogInterface;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.AsyncTask;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.example.shutdown_pc.tcp_client.TcpClient;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    private String SERVER_ADDRESS = "http://192.168.1.44:5000";
-    private String CONNECT_URL = SERVER_ADDRESS + "/connect";
-    private String SHUTDOWN_URL = SERVER_ADDRESS + "/shutdown";
-    private String CANCEL_SHUTDOWN_URL = SERVER_ADDRESS + "/shutdown_cancel";
-    private String SHUTDOWN_AFTER_URL = SERVER_ADDRESS + "/shutdown?time=";
-
+    private TcpClient tcpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        runConnectionWithServerChecker();
+
+        tcpClient = new TcpClient(new TcpClient.OnTcpEventHandler()
+                {
+                    @Override
+                    public void onChangeConnectionStatus(final boolean IsConnected)
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setStatus(IsConnected);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onMessageReceived(final String message)
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+        tcpClient.execute();
     }
 
     @Override
@@ -64,69 +75,63 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public boolean internetIsConnected()
-    {
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        return (networkInfo != null && networkInfo.isConnected());
-    }
-
-    public void onClickConnect(View view)
-    {
-        new ConnectTask().execute(CONNECT_URL);
-    }
-
     private Integer readTime()
     {
-        CheckBox useCustomTimeCB = (CheckBox) findViewById(R.id.useCustomTimeCB);
-        EditText timeField = (EditText) findViewById(R.id.customTimeField);
+        EditText customTimeField = (EditText) findViewById(R.id.customTimeField);
         Spinner defaultTimeSpinner = (Spinner) findViewById(R.id.timeSpinner);
 
         String time;
-        if (useCustomTimeCB.isChecked()) 
-			time = timeField.getText().toString();
+        if (customTimeField.getVisibility() == View.VISIBLE)
+			time = customTimeField.getText().toString();
         else 
 			time = defaultTimeSpinner.getSelectedItem().toString();
 
         if (time.isEmpty()) return -1;
-        return ((int) (Double.parseDouble(time) * 60));
+        return Integer.parseInt(time) * 60;
     }
 
     public void onClickShutdown(View view)
     {
         Integer time = readTime();
         if (time < 0) return;
-        new RequestTask().execute(SHUTDOWN_AFTER_URL + time);
+        tcpClient.sendMessage("shutdown " + time);
     }
 
     public void onClickUseCustomTime(View view)
     {
-        CheckBox useCustomTimeCB = (CheckBox) findViewById(R.id.useCustomTimeCB);
+        TextView useCustomTimeLabel = (TextView) findViewById(R.id.useCustomTimeLabel);
         Spinner defaultTimeSpinner = (Spinner) findViewById(R.id.timeSpinner);
-        if (useCustomTimeCB.isChecked())
-        {
-            setCustomTimeFieldEnabled(true);
-            defaultTimeSpinner.setEnabled(false);
-        }
-        else
-        {
-            setCustomTimeFieldEnabled(false);
-            defaultTimeSpinner.setEnabled(true);
-        }
+        ImageButton plusBtn = (ImageButton) findViewById(R.id.plusBtn);
+        ImageButton minusBtn = (ImageButton) findViewById(R.id.minusBtn);
+        EditText timeField = (EditText) findViewById(R.id.customTimeField);
+        useCustomTimeLabel.setVisibility(View.GONE);
+        defaultTimeSpinner.setVisibility(View.GONE);
+        plusBtn.setVisibility(View.GONE);
+        minusBtn.setVisibility(View.GONE);
+        timeField.setVisibility(View.VISIBLE);
+        String time = defaultTimeSpinner.getSelectedItem().toString();
+        timeField.setText(time);
     }
 
-	private void setCustomTimeFieldEnabled(boolean value)
-	{
-        EditText customTimeField = (EditText) findViewById(R.id.customTimeField);
-        customTimeField.setFocusable(value);
-        customTimeField.setClickable(value);
-        customTimeField.setFocusableInTouchMode(value);
-        customTimeField.setCursorVisible(value);
-	}
+    public void onClickPlusBtn(View view)
+    {
+        Spinner defaultTimeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        int curPosition = defaultTimeSpinner.getSelectedItemPosition();
+        if (defaultTimeSpinner.getResources().getStringArray(R.array.time).length > curPosition + 1)
+            defaultTimeSpinner.setSelection(curPosition + 1);
+    }
+
+    public void onClickMinusBtn(View view)
+    {
+        Spinner defaultTimeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        int curPosition = defaultTimeSpinner.getSelectedItemPosition();
+        if (curPosition - 1 >= 0)
+            defaultTimeSpinner.setSelection(curPosition - 1);
+    }
 
     public void onClickCancelShutdown(View view)
     {
-        new RequestTask().execute(CANCEL_SHUTDOWN_URL);
+        tcpClient.sendMessage("Cancel");
     }
 
     public void onClickExit(View view)
@@ -135,16 +140,15 @@ public class MainActivity extends AppCompatActivity
         System.exit(0);
     }
 
-    private void setServerAddress(String ServerAddress)
+    private void setServerAddress(String serverAddress)
     {
-        if (ServerAddress == null || ServerAddress.isEmpty()) return;
-        SERVER_ADDRESS = ServerAddress;
+        tcpClient.setServerAddress(serverAddress);
     }
 
     public void onServerSettingOptionsMenu(MenuItem item)
     {
         final EditText newHost = new EditText(this);
-        newHost.setText(SERVER_ADDRESS);
+        newHost.setText(tcpClient.getServerAddress());
         AlertDialog.Builder builder = new AlertDialog.Builder(this)
                 .setTitle(R.string.change_host_address)
                 .setView(newHost)
@@ -155,7 +159,6 @@ public class MainActivity extends AppCompatActivity
                     {
                         dialog.dismiss();
                         setServerAddress(newHost.getText().toString());
-                        new ConnectTask().execute(CONNECT_URL);
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
@@ -167,73 +170,5 @@ public class MainActivity extends AppCompatActivity
                     }
                 });
         builder.show();
-    }
-
-    private class ConnectTask extends RequestTask
-    {
-        protected void onPostExecute(String result)
-        {
-            if (result != null && result.equals("True"))
-            {
-                setStatus(true);
-            }
-            else
-            {
-                setStatus(false);
-            }
-        }
-    }
-
-    private class RequestTask extends AsyncTask<String, Integer, String>
-    {
-        @Override
-        protected String doInBackground(String... path)
-        {
-            try
-            {
-                return makeRequest(path[0]);
-            } catch (IOException ex)
-            {
-                return null;
-            }
-        }
-    }
-
-    private void runConnectionWithServerChecker()
-    {
-        final Handler handler = new Handler();
-        handler.post(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                new ConnectTask().execute(CONNECT_URL);
-                handler.postDelayed(this, 30000);
-            }
-        });
-    }
-
-    private String makeRequest(String path) throws IOException
-    {
-        if (!internetIsConnected()) return null;
-
-        URL url = new URL(path);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-        return readAnswer(connection);
-    }
-
-    private String readAnswer(HttpURLConnection connection) throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder buf = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null)
-        {
-            buf.append(line);
-        }
-        reader.close();
-        return buf.toString();
     }
 }
---
+++ shutdown-pc-android-client/app/src/main/java/com/example/shutdown_pc/tcp_client/TcpClient.java
@@ -0,0 +1,96 @@
+package com.example.shutdown_pc.tcp_client;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class TcpClient extends AsyncTask< Void, Void, Void >
+{
+    private Socket socket;
+    private String serverAddress = "192.168.1.45";
+    private int port = 9090;
+    private OnTcpEventHandler eventListener = null;
+
+    public TcpClient(OnTcpEventHandler eventListener)
+    {
+        this.eventListener = eventListener;
+    }
+
+    public void setServerAddress(String address)
+    {
+        this.serverAddress = address;
+        try
+        {
+            socket.close();
+        } catch (IOException e)
+        {
+            eventListener.onChangeConnectionStatus(false);
+        }
+    }
+
+    public String getServerAddress()
+    {
+        return serverAddress;
+    }
+
+    public void sendMessage(final String message)
+    {
+        Runnable runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    if (socket == null || socket.isClosed() || !socket.isConnected()) return;
+                    OutputStream output  = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(output, true);
+                    writer.println(message);
+                } catch (IOException e)
+                {
+                    eventListener.onChangeConnectionStatus(false);
+                    System.err.println(e);
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+    protected Void doInBackground(Void...param) {
+        while (true) {
+            try {
+                socket = new Socket(serverAddress, port);
+                eventListener.onChangeConnectionStatus(true);
+                InputStream input = socket.getInputStream();
+                InputStreamReader reader = new InputStreamReader(input);
+                int character;
+                StringBuilder data = new StringBuilder();
+                while ((character = reader.read()) != -1)
+                {
+                    data.append((char) character);
+                    if (character == '\n')
+                    {
+                        eventListener.onMessageReceived(data.toString());
+                        data.setLength(0);
+                    }
+                }
+                socket.close();
+            } catch (IOException e) {
+                eventListener.onChangeConnectionStatus(false);
+            }
+        }
+    }
+
+    public interface OnTcpEventHandler
+    {
+        void onChangeConnectionStatus(boolean IsConnected);
+        void onMessageReceived(String message);
+    }
+}
